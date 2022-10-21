e6ab0c4ed2e684b7785610fbbe0a3b011f961b51
Clove-lang <adedigbatimilehin@gmail.com>
2022-10-21 14:37:54 -0700

    Task 0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#inclu
 
  /**
   * main - Entry point
@@ -7,11 +7,8 @@
   *
   */
 
-int main(void)                                                                                                                              
-
-{                                                                                                                                           
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
